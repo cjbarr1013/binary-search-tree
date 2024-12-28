17c45cdaf82b4afabd5eea1c7ee21341e8eb6df4
Cameron Barr <cjbarr1013@gmail.com>
2024-12-28 10:59:59 -0500

    Prettify console output

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ function logNodeValue(node) {
   console.log(node.value);
 }
 
+function printSeparator(msg) {
+  console.log();
+  console.log(msg);
+  console.log('-------------------------');
+  console.log();
+}
+
 const myArr = [17, 62, 78, 23, 8, 21, 65, 3, 32, 7, 98, 67, 49, 12, 8];
 const tree = new Tree(myArr);
 
+printSeparator('Original tree');
 tree.printTree();
 
 tree.insert(8);
 tree.insert(43);
 tree.insert(97);
 
+printSeparator('Insert several values');
 tree.printTree();
 
 tree.deleteItem(32);
 
+printSeparator('Delete value(s)');
 tree.printTree();
 
+printSeparator('Find node given value');
 console.log(tree.find(67));
 
+printSeparator('Level Order Traversal');
 tree.levelOrder(logNodeValue);
